8d44f2344a7a70c1a701ad27faf8fa29647c0ecb
RexitEricSia <sia.delong@rexit.com>
2025-04-10 09:05:24 +0800

    added distribution guide

--- src/App.tsx
+++ src/App.tsx
@@ -9,24 +9,38 @@ import List from "./component/List";
 import NavBar from "./component/NavBar";
 import { SwitchProvider } from "./context/Provider/SwitchProvider";
 
+// // Lazy load components
+// import { lazy } from "react";
+// import { Suspense } from "react";
+// const Counter = lazy(() => import("./component/Counter"));
+// const Home = lazy(() => import("./page/Home"));
+// const Info = lazy(() => import("./page/Info"));
+// const Profile = lazy(() => import("./page/Profile"));
+// const Layout = lazy(() => import("./Layout"));
+// const State = lazy(() => import("./component/State"));
+// const List = lazy(() => import("./component/List"));
+// const NavBar = lazy(() => import("./component/NavBar"));
+
 function App() {
 
   return (
     <Router>
       <SwitchProvider>
-        <Routes>
-          <Route element={<Layout />}>
-            <Route path="/counter" element={<Counter />} />
-            <Route path="/state" element={<State />} />
-            <Route path="/list" element={<List />} />
-            <Route element={<NavBar />}>
-              <Route path="/home" element={<Home />} />
-              <Route path="/info" element={<Info />} />
-              <Route path="/profile" element={<Profile />} />
+        {/* <Suspense fallback={<div>Loading...</div>}> */}
+          <Routes>
+            <Route element={<Layout />}>
+              <Route path="/counter" element={<Counter />} />
+              <Route path="/state" element={<State />} />
+              <Route path="/list" element={<List />} />
+              <Route element={<NavBar />}>
+                <Route path="/home" element={<Home />} />
+                <Route path="/info" element={<Info />} />
+                <Route path="/profile" element={<Profile />} />
+              </Route>
+              <Route path="*" element={<State />} />
             </Route>
-            <Route path="*" element={<State />} />
-          </Route>
-        </Routes>
+          </Routes>
+        {/* </Suspense> */}
       </SwitchProvider>
     </Router >
   )
--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,19 @@
-import { defineConfig } from 'vite'
+import { defineConfig, PluginOption } from 'vite'
 import react from '@vitejs/plugin-react'
+import { visualizer } from 'rollup-plugin-visualizer';
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), visualizer() as PluginOption],
+  build: {
+    rollupOptions: {
+      output: {
+        manualChunks(id) {
+          if (id.includes('react-router-dom')) {
+            return 'react-router-dom';
+          }
+        }
+      }
+    }
+  }
 })
